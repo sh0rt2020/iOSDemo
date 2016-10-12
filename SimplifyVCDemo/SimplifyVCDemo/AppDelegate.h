9a44c4aeb5c17cab5b106327cc1cd31d957cb155
Cr02y <waynesun1990@gmail.com>
2016-10-12 11:50:50 +0800

    init a new project

---
+++ SimplifyVCDemo/SimplifyVCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimplifyVCDemo
+//
+//  Created by sunwell on 2016/10/12.
+//  Copyright © 2016年 com.sun.mantledemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimplifyVCDemo/SimplifyVCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimplifyVCDemo
+//
+//  Created by sunwell on 2016/10/12.
+//  Copyright © 2016年 com.sun.mantledemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
