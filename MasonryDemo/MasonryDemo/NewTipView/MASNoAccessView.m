477de29f54203ceb3e97e3e90f10f118bcfd6f2a
Cr02y <waynesun1990@gmail.com>
2016-10-27 21:49:55 +0800

    complete NewNoAccessView file

---
+++ MasonryDemo/MasonryDemo/NewTipView/MASNoAccessView.m
@@ -0,0 +1,21 @@
+//
+//  MASNoAccessView.m
+//  NoAccessDemo
+//
+//  Created by Yige on 2016/10/27.
+//  Copyright © 2016年 Yige. All rights reserved.
+//
+
+#import "MASNoAccessView.h"
+
+@implementation MASNoAccessView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
