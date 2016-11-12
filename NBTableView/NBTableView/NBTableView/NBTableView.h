51139eb91788f7e77655605ba66822a987fd8736
Cr02y <waynesun1990@gmail.com>
2016-11-12 18:10:28 +0800

    delete a swift project

---
+++ NBTableView/NBTableView/NBTableView/NBTableView.h
@@ -0,0 +1,13 @@
+//
+//  NBTableView.h
+//  NBTableView
+//
+//  Created by sunwell on 2016/11/12.
+//  Copyright © 2016年 sunwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBTableView : UIScrollView
+
+@end
---
+++ NBTableView/NBTableView/NBTableView/NBTableView.m
@@ -0,0 +1,21 @@
+//
+//  NBTableView.m
+//  NBTableView
+//
+//  Created by sunwell on 2016/11/12.
+//  Copyright © 2016年 sunwell. All rights reserved.
+//
+
+#import "NBTableView.h"
+
+@implementation NBTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
