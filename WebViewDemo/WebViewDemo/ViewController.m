9ae82200518145bfe6fe0b2d368df2d854807b18
sunwell <waynesun1990@gmail.com>
2016-11-24 16:02:55 +0800

    js code invoke OC code.

--- WebViewDemo/WebViewDemo/ViewController.m
+++ WebViewDemo/WebViewDemo/ViewController.m
@@ -11,30 +11,55 @@
 
 #import "ViewController.h"
 #import <WebKit/WebKit.h>
+#import <WebViewJavascriptBridge.h>
+#import <JavaScriptCore/JavaScriptCore.h>
 
 @interface ViewController () <UIWebViewDelegate, WKUIDelegate>
 
 @property (nonatomic, nonnull) UIWebView *webView;
 //@property (nonatomic, nonnull) WKWebView *webView;
 @property (nonatomic, nonnull) UIButton *sizeBtn;  //改变大小的按钮
+@property (nonatomic)   WebViewJavascriptBridge *bridge;  //原生代码和js代码交互的桥接
 @end
 
 @implementation ViewController
 #pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
     self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 100, SCREENWIDTH, SCREENTHEIGHT-100)];
     self.webView.delegate =self;
 //    self.webView.UIDelegate = self;
-    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.github.com"]]];
+//    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.github.com"]]];
+    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"WebViewDemo" ofType:@"html"];
+    NSString *html = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
+    [self.webView loadHTMLString:html baseURL:nil];
     [self.view addSubview:self.webView];
+    
+    
     
     self.sizeBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, SCREENWIDTH, 100)];
     self.sizeBtn.backgroundColor = [UIColor greenColor];
     [self.sizeBtn addTarget:self action:@selector(changeFont:) forControlEvents:UIControlEventTouchUpInside];
     [self.sizeBtn setTitle:@"change font size" forState:UIControlStateNormal];
     [self.view addSubview:self.sizeBtn];
+    
+    [WebViewJavascriptBridge enableLogging];  //调试
+    self.bridge = [WebViewJavascriptBridge bridgeForWebView:self.webView];
+    [self.bridge setWebViewDelegate:self];
+    
+    
+    
+    
+    id data = @{};
+    [self.bridge callHandler:@"showYwDomainLogin" data:data responseCallback:^(id responseData) {
+        NSLog(@"OC invoke Js succeed!");
+    }];
+    
+    [self.bridge registerHandler:@"stopLocalMedia()" handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"Js invoke OC succeed!");
+    }];
 }
 
 
@@ -56,6 +81,8 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     NSLog(@"%s", __func__);
+//    [self injectJs];  //注入js
+//    [self interceptJs];
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
@@ -73,5 +100,30 @@
 //    CGRect newFrame = self.webView.frame;
 //    newFrame.size.height = scrollheight;
 //    self.webView.frame = newFrame;
+}
+
+
+- (void)showSFELoginPage {
+    
+    NSLog(@"%s", __func__);
+}
+
+
+#pragma mark - private method
+- (void)injectJs {
+    
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"WebViewDemo" ofType:@"js"];
+    [self.webView stringByEvaluatingJavaScriptFromString:
+     [NSString stringWithFormat:@"var script = document.createElement('script');"
+      "script.type = 'text/javascript';"
+      "script.src = '%@';"
+      "document.getElementsByTagName('head')[0].appendChild(script);",path]];
+}
+
+- (void)interceptJs {
+    JSContext *context = [self.webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    context[@"showYwDomainLogin"] = ^() {
+        NSLog(@"拦截js");
+    };
 }
 @end
---
+++ WebViewDemo/WebViewDemo/WebViewDemo.js
@@ -0,0 +1,21 @@
+
+function setupWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge); }
+    if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function() { document.documentElement.removeChild(WVJBIframe) }, 0)
+}
+
+setupWebViewJavascriptBridge(function(bridge) {
+                             
+    bridge.registerHandler('showYwDomainLogin', function(data, responseCallback) {
+        log('ObjC called Js')
+//        var responseData = { 'Javascript Says':'Right back atcha!' }
+//        log('JS responding with', responseData)
+//        responseCallback(responseData)
+    })
+})
