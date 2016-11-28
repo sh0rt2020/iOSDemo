6ac4b62f4b4645a491da7e1b11de79add9f3eadc
sunwell <waynesun1990@gmail.com>
2016-11-28 17:52:14 +0800

    add VideoDemo pro. And this is based on TencentSdk.
    Former-commit-id: 82cc816802243689478de90b818e04cb3298592e

---
+++ VideoDemo/VideoDemo/PlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayViewController.h
+//  VideoDemo
+//
+//  Created by Yige on 2016/11/28.
+//  Copyright © 2016年 Yige. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayViewController : UIViewController
+
+@end
---
+++ VideoDemo/VideoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoDemo
+//
+//  Created by Yige on 2016/11/28.
+//  Copyright © 2016年 Yige. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
