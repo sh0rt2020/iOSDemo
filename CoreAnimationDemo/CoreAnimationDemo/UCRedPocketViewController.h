91a941db04923f421f93f5ff874a4120a93c492e
Cr02y <waynesun1990@gmail.com>
2017-05-25 00:55:46 +0800

    del animate demo and update CoreAnimatation demo.

---
+++ CoreAnimationDemo/CoreAnimationDemo/UCRedPocketViewController.h
@@ -0,0 +1,13 @@
+//
+//  UCRedPocketViewController.h
+//  CoreAnimationDemo
+//
+//  Created by sunwell on 2017/5/25.
+//  Copyright © 2017年 sunwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UCRedPocketViewController : UIViewController
+
+@end
---
+++ CoreAnimationDemo/CoreAnimationDemo/UCRedPocketViewController.m
@@ -0,0 +1,37 @@
+//
+//  UCRedPocketViewController.m
+//  CoreAnimationDemo
+//
+//  Created by sunwell on 2017/5/25.
+//  Copyright © 2017年 sunwell. All rights reserved.
+//
+
+#import "UCRedPocketViewController.h"
+
+@interface UCRedPocketViewController ()
+
+@end
+
+@implementation UCRedPocketViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
