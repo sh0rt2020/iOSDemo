f38d53b9acbc3f685e25e9e41a13ce19bfc0aa35
Cr02y <waynesun1990@gmail.com>
2017-05-24 23:37:09 +0800

    add a coreanimation demo.

---
+++ CoreAnimationDemo/CoreAnimationDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  CoreAnimationDemo
+//
+//  Created by sunwell on 2017/5/24.
+//  Copyright © 2017年 sunwell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
