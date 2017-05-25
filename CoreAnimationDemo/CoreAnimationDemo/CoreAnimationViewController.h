edafe01f47e00aba2206f6147c963f39b04c0688
Cr02y <waynesun1990@gmail.com>
2017-05-26 00:34:39 +0800

    fix bug.

--- CoreAnimationDemo/CoreAnimationDemo/CoreAnimationViewController.h
+++ CoreAnimationDemo/CoreAnimationDemo/CoreAnimationViewController.h
@@ -5,9 +5,8 @@
 //  Created by iosdevlope on 2017/5/25.
 //  Copyright © 2017年 sunwell. All rights reserved.
 //
+#import <UIKit/UIKit.h>
 
-#import "ViewController.h"
-
-@interface CoreAnimationViewController : ViewController
+@interface CoreAnimationViewController : UIViewController
 
 @end
