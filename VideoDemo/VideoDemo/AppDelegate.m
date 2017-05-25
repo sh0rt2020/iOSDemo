57e63355d57aac669cea48703c52699cfce6b2a7
51gif <waynesun1990@gmail.com>
2017-05-25 09:44:07 +0800

    update video demo

--- VideoDemo/VideoDemo/AppDelegate.m
+++ VideoDemo/VideoDemo/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "GSNewPlayerViewController.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -20,7 +21,8 @@
     // Override point for customization after application launch.
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    GSNewPlayerViewController *vc = [[GSNewPlayerViewController alloc] init];
+//    GSNewPlayerViewController *vc = [[GSNewPlayerViewController alloc] init];
+    ViewController *vc = [[ViewController alloc] init];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
