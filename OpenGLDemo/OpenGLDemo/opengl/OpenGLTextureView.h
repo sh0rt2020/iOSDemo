e7262bc9e185be032831844d8978d8613041d41f
51gif <waynesun1990@gmail.com>
2017-06-27 13:52:06 +0800

    add texture based on texture png

---
+++ OpenGLDemo/OpenGLDemo/opengl/OpenGLTextureView.h
@@ -0,0 +1,13 @@
+//
+//  OpenGLTextureView.h
+//  OpenGLDemo
+//
+//  Created by iosdevlope on 2017/6/27.
+//  Copyright © 2017年 iosdevlope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGLTextureView : UIView
+
+@end
--- OpenGLDemo/OpenGLDemo/opengl/SimpleFragment.glsl
+++ OpenGLDemo/OpenGLDemo/opengl/SimpleFragment.glsl
@@ -1,6 +1,15 @@
+//
+//varying lowp vec4 DestinationColor; // 1
+//
+//void main(void) { // 2
+//    gl_FragColor = DestinationColor; // 3
+//}
 
-varying lowp vec4 DestinationColor; // 1
+varying lowp vec4 DestinationColor;
 
-void main(void) { // 2
-    gl_FragColor = DestinationColor; // 3
+varying lowp vec2 TexCoordOut; // New
+uniform sampler2D Texture; // New
+
+void main(void) {
+    gl_FragColor = DestinationColor * texture2D(Texture, TexCoordOut); // New
 }
